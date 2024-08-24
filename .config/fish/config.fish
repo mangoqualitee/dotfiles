43d1681c8c241069fe54462d76f0e104f622c7f7
vishal <95016059+gyanantaran@users.noreply.github.com>
2024-08-24 22:06:51 +0530

    [maintenance] removed bug causers and minor annoyances

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,18 @@
 set fish_greeting
-fish_vi_key_bindings
-
 if status is-interactive
-    # Homebrew initialization
-    test -d /opt/homebrew && set -gx HOMEBREW_PREFIX /opt/homebrew
-    test -d "/home/linuxbrew/.linuxbrew" && set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
-    eval ($HOMEBREW_PREFIX/bin/brew shellenv)
-    test -x (command -v brew) || echo "brew not on path"
-
     # Load lang-toolchains
     test -d "$HOME/.cargo/bin" && set -x PATH "$HOME/.cargo/bin" $PATH; or echo ".cargo not on path"
     test -d "$HOME/.juliaup/bin" && set -gx PATH "$HOME/.juliaup/bin" $PATH; or echo ".juliaup not on path"
     test -d "$HOME/.miniforge3/bin" && $HOME/.miniforge3/bin/conda "shell.fish" hook | source; or echo ".miniforge not on path"
 
     # Load utilities
-    test -x (command -v fzf) && fzf --fish | source
+    test -x (command -v fzf) && fzf --fish | source; or echo "fzf not on path"
     test -x (command -v trash) && abbr rm trash; or echo "trash not on path"
 
     # Exports and Aliases
     set -gx PATH "$HOME/.local/bin" $PATH
-    set -gx EDITOR /home/linuxbrew/.linuxbrew/bin/hx && abbr hx \$EDITOR
-    # set -gx LANG en_IN.UTF8
+    set -gx EDITOR /usr/bin/helix && abbr hx \$EDITOR
     set -gx LESSHISTFILE -
-
+    # set -gx LANG en_US.UTF8
     #alias jupyterlisten 'ssh -NL localhost:1234:localhost:8888 fbd'
 end
