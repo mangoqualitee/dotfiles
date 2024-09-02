fbc5ed3128bb1f95d2ab261f7ff5ea31caf8c874
vishal <95016059+gyanantaran@users.noreply.github.com>
2024-09-03 04:09:56 +0530

    [format] removed bloat

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,18 @@
-set fish_greeting
 if status is-interactive
+    # Drop habits
+    set -gx beep 'bash -c "paplay /home/vishal/Music/beep.mp3 &"'
+    bind \cl $beep
+    alias clear $beep
     # Load lang-toolchains
-    test -d "$HOME/.cargo/bin" && set -x PATH "$HOME/.cargo/bin" $PATH; or echo ".cargo not on path"
-    test -d "$HOME/.juliaup/bin" && set -gx PATH "$HOME/.juliaup/bin" $PATH; or echo ".juliaup not on path"
-    test -d "$HOME/.miniforge3/bin" && $HOME/.miniforge3/bin/conda "shell.fish" hook | source; or echo ".miniforge not on path"
-
+    test -d $HOME/.cargo/bin && set -x PATH $HOME/.cargo/bin $PATH
+    test -d $HOME/.juliaup/bin && set -gx PATH $HOME/.juliaup/bin $PATH
+    test -d $HOME/.miniforge3/bin && $HOME/.miniforge3/bin/conda "shell.fish" hook | source
     # Load utilities
-    test -x (command -v fzf) && fzf --fish | source; or echo "fzf not on path"
-    test -x (command -v trash) && abbr rm trash; or echo "trash not on path"
-
+    test -x (command -v trash) && abbr rm trash
     # Exports and Aliases
-    set -gx PATH "$HOME/.local/bin" $PATH
-    set -gx EDITOR /usr/bin/helix && abbr hx \$EDITOR
+    set -gx PATH $HOME/.local/bin $PATH
+    set -gx EDITOR /usr/bin/helix && abbr hx '$EDITOR'
     set -gx LESSHISTFILE -
-    # set -gx LANG en_US.UTF8
-    #alias jupyterlisten 'ssh -NL localhost:1234:localhost:8888 fbd'
+    # Move prompt bottom
+    tput cup "$LINES"
 end
