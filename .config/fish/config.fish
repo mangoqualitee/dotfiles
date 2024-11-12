2e91f1593044a59c614154dc0b0971725255b9f7
vishal <95016059+gyanantaran@users.noreply.github.com>
2024-11-13 02:35:13 +0530

    Minor changes. themes added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,90 @@
-if status is-interactive
-    # Drop habits
-    set -gx beep 'bash -c "paplay /home/vishal/Music/beep.mp3 &"'
-    bind \cl $beep
-    alias clear $beep
-    # Load lang-toolchains
-    test -d $HOME/.cargo/bin && set -x PATH $HOME/.cargo/bin $PATH
-    test -d $HOME/.juliaup/bin && set -gx PATH $HOME/.juliaup/bin $PATH
-    test -d $HOME/.miniforge3/bin && $HOME/.miniforge3/bin/conda "shell.fish" hook | source
-    # Load utilities
-    test -x (command -v trash) && abbr rm trash
-    # Exports and Aliases
-    set -gx PATH $HOME/.local/bin $PATH
-    set -gx PATH /opt/cuda/bin $PATH
-    set -gx LD_LIBRARY_PATH /opt/cuda/lib64/ $LD_LIBRARY_PATH
-    set -gx EDITOR /usr/bin/helix && abbr hx '$EDITOR'
-    set -gx LESSHISTFILE -
-    # Move prompt bottom
-    tput cup "$LINES"
+abbr -a o xdg-open
+abbr -a g git
+abbr -a gc 'git checkout'
+abbr -a ga 'git add -p'
+abbr -a p 'sudo pacman'
+abbr -a up 'sudo pacman -Syu'
+abbr -a rm trash
+abbr -a hx '$EDITOR'
+
+if command -v eza >/dev/null
+    abbr -a l eza
+    abbr -a ls eza
+    abbr -a ll 'eza -l'
+    abbr -a lll 'eza -la'
+else
+    abbr -a l ls
+    abbr -a ll 'ls -l'
+    abbr -a lll 'ls -la'
 end
 
+# Load lang-toolchains
+source "$HOME/.cargo/env.fish"
+$HOME/.miniforge3/bin/conda "shell.fish" hook | source
+test -d $HOME/.juliaup/bin && set -gx PATH $HOME/.juliaup/bin $PATH
+# Exports and Aliases
+set -gx PATH $HOME/.local/bin $PATH
+#set -gx PATH /opt/cuda/bin $PATH
+#set -gx LD_LIBRARY_PATH /opt/cuda/lib64/ $LD_LIBRARY_PATH
+set -gx LESSHISTFILE -
+set -gx EDITOR /usr/bin/helix
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 set -gx PATH $HOME/.cabal/bin $PATH /home/vishal/.ghcup/bin # ghcup-env
+
+
+if status is-interactive
+end
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles yes
+set __fish_git_prompt_showdirtystate yes
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream none
+set -g fish_prompt_pwd_dir_length 3
+
+function fish_prompt
+    # Move prompt bottom
+    tput cup "$LINES"
+    # Rest of the prompt
+    set_color brblack
+    echo -n "["(date "+%H:%M")"] "
+    set_color blue
+    echo -n (hostnamectl hostname)
+    if [ $PWD != $HOME ]
+        set_color brblack
+        echo -n ':'
+        set_color yellow
+        echo -n (basename $PWD)
+    end
+    set_color green
+    printf '%s ' (__fish_git_prompt)
+    set_color red
+    echo -n '| '
+    set_color normal
+end
+
+if test -f /usr/share/autojump/autojump.fish
+    source /usr/share/autojump/autojump.fish
+end
+
+# colored man output
+# from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+setenv LESS_TERMCAP_mb \e'[01;31m' # begin blinking
+setenv LESS_TERMCAP_md \e'[01;38;5;74m' # begin bold
+setenv LESS_TERMCAP_me \e'[0m' # end mode
+setenv LESS_TERMCAP_se \e'[0m' # end standout-mode
+setenv LESS_TERMCAP_so \e'[38;5;246m' # begin standout-mode - info box
+setenv LESS_TERMCAP_ue \e'[0m' # end underline
+setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+# fzf
+setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
+setenv FZF_CTRL_T_COMMAND 'fd --type file --follow'
+setenv FZF_DEFAULT_OPTS '--height 20%'
+
+function fish_user_key_bindings
+    bind \cz 'fg>/dev/null ^/dev/null'
+    if functions -q fzf_key_bindings
+        fzf_key_bindings
+    end
+end
