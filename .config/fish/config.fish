8b96b3162fdb706b3b555f0f81cedc70cd753ddc
vishal <95016059+gyanantaran@users.noreply.github.com>
2025-01-25 22:05:39 +0530

    Ditching alacritty, tmux; for ghostty, zellij

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,10 +38,10 @@ setenv FZF_DEFAULT_OPTS '--height 20%'
 # fish_vi_key_bindings
 
 function fish_user_key_bindings
-    # bind \cl 'echo bad'
     alias clear 'echo bad'
     # bind \cz 'fg>/dev/null ^/dev/null'
     bind \ck kill-line
+    bind \cl 'echo bad'
     if functions -q fzf_key_bindings
         fzf_key_bindings
     end
@@ -56,30 +56,4 @@ function y
     rm -f -- "$tmp"
 end
 
-# Fish git prompt
-set __fish_git_prompt_showuntrackedfiles yes
-set __fish_git_prompt_showdirtystate yes
-set __fish_git_prompt_showstashstate ''
-set __fish_git_prompt_showupstream none
-set -g fish_prompt_pwd_dir_length 3
-
-function fish_prompt
-    # Move prompt bottom
-    # tput cup "$LINES"
-    # Rest of the prompt
-    # set_color brblack
-    # echo -n "["(date "+%H:%M")"] "
-    set_color blue
-    echo -n (hostnamectl hostname)
-    if [ $PWD != $HOME ]
-        set_color brblack
-        echo -n ':'
-        set_color yellow
-        echo -n (basename $PWD)
-    end
-    set_color green
-    printf '%s ' (__fish_git_prompt)
-    set_color red
-    echo -n '| '
-    set_color normal
-end
+source (/usr/bin/starship init fish --print-full-init | psub)
