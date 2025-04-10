8b54fccb3fc0f8ea1f9bf8abaf9f9821ffe87b4a
vishal <95016059+gyanantaran@users.noreply.github.com>
2025-04-10 11:04:04 +0530

    mntn: themes model changed, nuke on sway modular

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,73 +1,60 @@
-abbr -a o xdg-open
-abbr -a g git
-abbr -a ga 'git add -p'
-abbr -a gc 'git checkout'
-abbr -a p 'sudo pacman'
-abbr -a pi 'sudo pacman -S'
-abbr -a pu 'sudo pacman -Syu'
-abbr -a rm trash
-abbr -a hx '$EDITOR'
-
-if command -v eza >/dev/null
-    abbr -a l eza
-    abbr -a ls eza
-    abbr -a ll 'eza -l'
-    abbr -a la 'eza -la'
-end
-
-# Load lang-toolchains
-source "$HOME/.cargo/env.fish"
-$HOME/.miniforge3/bin/conda "shell.fish" hook | source
-test -d $HOME/.juliaup/bin && set -gx PATH $HOME/.juliaup/bin $PATH
-
-# Exports and Aliases
-set -gx PATH /home/vishal/.local/share/gem/ruby/3.3.0/bin $PATH
-set -gx PATH $HOME/.local/bin $PATH
-set -gx PATH $HOME/.config/zide/bin $PATH
-#set -gx PATH /opt/cuda/bin $PATH
-#set -gx LD_LIBRARY_PATH /opt/cuda/lib64/ $LD_LIBRARY_PATH
-set -gx LESSHISTFILE -
-set -gx EDITOR /home/vishal/.cargo/bin/hx
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-set -gx PATH $HOME/.cabal/bin $PATH /home/vishal/.ghcup/bin # ghcup-env
-set -gx HELIX_RUNTIME $HOME/dev/open-source/tools/helix/runtime
-# set -gx BROWSER /usr/bin/vivaldi-stable
-
-# fish_vi_key_bindings
-function fish_user_key_bindings
-    # bind \cz 'fg>/dev/null ^/dev/null'
-    bind \ck kill-line
-    bind \cl 'commandline ""'
-    bind \el y
-    bind \cc 'commandline ""' # no new line on C-c
-    alias clear 'commandline ""' # no new line on C-c
-    if functions -q fzf_key_bindings
-        fzf_key_bindings
-    end
-end
-
-alias y yazi
-# function y
-#     set tmp (mktemp -t "yazi-cwd.XXXXXX")
-#     yazi $argv --cwd-file="$tmp"
-#     if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-#         builtin cd -- "$cwd"
-#     end
-#     rm -f -- "$tmp"
-# end
-
-source (/usr/bin/starship init fish --print-full-init | psub)
-zoxide init fish | source
-
 if status --is-interactive
-    # fzf
-    setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
-    setenv FZF_CTRL_T_COMMAND 'fd --type file --follow'
-    setenv FZF_DEFAULT_OPTS '--height 20%'
+    abbr -a g  git
+    abbr -a ga 'git add -p'
+    abbr -a gc 'git checkout'
+    abbr -a hx '$EDITOR'
+    abbr -a l  eza
+    abbr -a la 'eza -la'
+    abbr -a ll 'eza -l'
+    abbr -a ls eza
+    abbr -a o  xdg-open
+    abbr -a p  'sudo pacman'
+    abbr -a pi 'sudo pacman -S'
+    abbr -a pu 'sudo pacman -Syu'
+    abbr -a rm trash
+    abbr -a wd 'cd (cat /home/vishal/workingdir)'
+    abbr -a y yazi
 
-    if test -d ~/dev/others/base16/templates/fish-shell
-        #ls ~/dev/others/base16/templates/fish-shell/functions/base16-*.fish | sed -E 's/.*base16-(.*).fish/\1/g'
-        set fish_function_path $fish_function_path ~/dev/others/base16/templates/fish-shell/functions
-        builtin source ~/dev/others/base16/templates/fish-shell/conf.d/base16.fish
+    # lang-toolchains
+    set -gx PATH "$HOME/.miniconda/bin" $PATH
+    set -gx PATH "$HOME/.cargo/bin" $PATH
+# set -gx PATH "$HOME/.miniforge3/bin" $PATH  # commented out by conda initialize
+    set -gx PATH "$HOME/.juliaup/bin" $PATH
+    set -gx PATH "$HOME/.local/bin" $PATH
+    set -gx PATH "$HOME/node_modules/vscode-json-languageserver/bin" $PATH
+
+    #set -gx PATH /opt/cuda/bin $PATH
+    #set -gx LD_LIBRARY_PATH /opt/cuda/lib64/ $LD_LIBRARY_PATH
+
+    # setenv LESSHISTFILE -
+    setenv HELIX_RUNTIME $HOME/dev/open-source/tools/helix/runtime
+    setenv EDITOR /home/vishal/.local/bin/hx
+    setenv BROWSER /usr/bin/vivaldi-stable
+
+    function fish_user_key_bindings
+        # fish_vi_key_bindings
+        bind \cz 'fg 2>/dev/null; commandline -f repaint'
+        bind \ck kill-line
+        bind \el y
+        # bind \cc 'commandline ""'
+        bind \cl 'commandline ""'
+        alias clear 'commandline ""'
     end
+
+    # source (/usr/bin/starship init fish --print-full-init | psub)
+    zoxide init fish | source
+    fzf --fish | source
+
+    # Fish git prompt
+    set __fish_git_prompt_showuntrackedfiles yes
+    set __fish_git_prompt_showdirtystate yes
+    set __fish_git_prompt_showstashstate ''
+    set __fish_git_prompt_showupstream none
+    set -g fish_prompt_pwd_dir_length 1
+
+    # theming
+    # source $HOME/dev/others/base16/templates/tinted-fzf/fish/base16-gruvbox-dark-hard.fish
+    # source $HOME/dev/others/base16/templates/base16-shell/profile_helper.fish
+    # base16-gruvbox-dark-hard
+
 end
